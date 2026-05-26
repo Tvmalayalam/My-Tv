002445f1d166ad4834f91833a36f9f5b62cc449a
Tvmalayalam <turbolevelgaming@gmail.com>
2026-05-26 15:24:26 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -3,43 +3,35 @@ import urllib.request
 try:
     m3u_content = "#EXTM3U\n\n"
     
-    # 2026-ലെ ഏറ്റവും പുതിയതും tulnit സൈറ്റിലുള്ളതുമായ മുഴുവൻ മലയാളം ചാനലുകളുടെയും ലിസ്റ്റ്
+    # ബ്ലോക്ക് ആകാത്ത ഒറിജിനൽ ലോഗോകളും ഡയറക്ട് സ്ട്രീമും അടങ്ങിയ ലിസ്റ്റ്
     channels = [
-        {"name": "Asianet HD", "id": "starasianet1_live_https", "logo": "https://i.imgur.com/vHtwV66.png"},
-        {"name": "Asianet Movies HD", "id": "asianetmovies_live_https", "logo": "https://i.imgur.com/GZp8S6O.png"},
-        {"name": "Asianet Plus", "id": "asianetplus_live_https", "logo": "https://i.imgur.com/K3Z0g8G.png"},
-        {"name": "Surya TV HD", "id": "suryatv_live_https", "logo": "https://i.imgur.com/xK9fXmO.png"},
-        {"name": "Surya Movies", "id": "suryamovies_live_https", "logo": "https://i.imgur.com/v8bZp6N.png"},
-        {"name": "Surya Comedy", "id": "suryacomedy_live_https", "logo": "https://i.imgur.com/w9vB7zX.png"},
-        {"name": "Surya Music", "id": "suryamusic_live_https", "logo": "https://i.imgur.com/z8pM7vX.png"},
-        {"name": "Kochu TV", "id": "kochutv_live_https", "logo": "https://i.imgur.com/v7xB5zM.png"},
-        {"name": "Mazhavil Manorama HD", "id": "mmtv_live_https", "logo": "https://i.imgur.com/h9vX4zK.png"},
-        {"name": "Flowers TV HD", "id": "flowerstv_live_https", "logo": "https://i.imgur.com/m9bV4xZ.png"},
-        {"name": "Asianet News HD", "id": "asianetnews_live_https", "logo": "https://i.imgur.com/8zX8v7G.png"},
-        {"name": "24 News HD", "id": "twentytofournews_live_https", "logo": "https://i.imgur.com/v7zM4bX.png"},
-        {"name": "Mathrubhumi News", "id": "mbnews_live_https", "logo": "https://i.imgur.com/w8vX4zM.png"},
-        {"name": "News18 Kerala", "id": "news18kerala_live_https", "logo": "https://i.imgur.com/z9bV4xZ.png"},
-        {"name": "MediaOne", "id": "mediaone_live_https", "logo": "https://i.imgur.com/x8vX7zK.png"},
-        {"name": "Janam TV", "id": "janamtv_live_https", "logo": "https://i.imgur.com/v9zM4bX.png"}
+        {"name": "Asianet HD", "url": "https://anettv.tulnit.workers.dev/starasianet1_live_https/index.m3u8", "logo": "https://raw.githubusercontent.com/manishb20/Tv-logos/main/asianet.png"},
+        {"name": "Asianet Movies HD", "url": "https://anettv.tulnit.workers.dev/asianetmovies_live_https/index.m3u8", "logo": "https://raw.githubusercontent.com/manishb20/Tv-logos/main/asianetmovies.png"},
+        {"name": "Asianet Plus", "url": "https://anettv.tulnit.workers.dev/asianetplus_live_https/index.m3u8", "logo": "https://raw.githubusercontent.com/manishb20/Tv-logos/main/asianetplus.png"},
+        {"name": "Surya TV HD", "url": "https://anettv.tulnit.workers.dev/suryatv_live_https/index.m3u8", "logo": "https://raw.githubusercontent.com/manishb20/Tv-logos/main/suryatv.png"},
+        {"name": "Surya Movies", "url": "https://anettv.tulnit.workers.dev/suryamovies_live_https/index.m3u8", "logo": "https://raw.githubusercontent.com/manishb20/Tv-logos/main/suryamovies.png"},
+        {"name": "Surya Comedy", "url": "https://anettv.tulnit.workers.dev/suryacomedy_live_https/index.m3u8", "logo": "https://raw.githubusercontent.com/manishb20/Tv-logos/main/suryacomedy.png"},
+        {"name": "Surya Music", "url": "https://anettv.tulnit.workers.dev/suryamusic_live_https/index.m3u8", "logo": "https://raw.githubusercontent.com/manishb20/Tv-logos/main/suryamusic.png"},
+        {"name": "Kochu TV", "url": "https://anettv.tulnit.workers.dev/kochutv_live_https/index.m3u8", "logo": "https://raw.githubusercontent.com/manishb20/Tv-logos/main/kochutv.png"},
+        {"name": "Mazhavil Manorama HD", "url": "https://anettv.tulnit.workers.dev/mmtv_live_https/index.m3u8", "logo": "https://raw.githubusercontent.com/manishb20/Tv-logos/main/mazhavilmanorama.png"},
+        {"name": "Flowers TV HD", "url": "https://anettv.tulnit.workers.dev/flowerstv_live_https/index.m3u8", "logo": "https://raw.githubusercontent.com/manishb20/Tv-logos/main/flowerstv.png"},
+        {"name": "Asianet News HD", "url": "https://anettv.tulnit.workers.dev/asianetnews_live_https/index.m3u8", "logo": "https://raw.githubusercontent.com/manishb20/Tv-logos/main/asianetnews.png"},
+        {"name": "24 News HD", "url": "https://anettv.tulnit.workers.dev/twentytofournews_live_https/index.m3u8", "logo": "https://raw.githubusercontent.com/manishb20/Tv-logos/main/24news.png"},
+        {"name": "Mathrubhumi News", "url": "https://anettv.tulnit.workers.dev/mbnews_live_https/index.m3u8", "logo": "https://raw.githubusercontent.com/manishb20/Tv-logos/main/mathrubhuminews.png"},
+        {"name": "News18 Kerala", "url": "https://anettv.tulnit.workers.dev/news18kerala_live_https/index.m3u8", "logo": "https://raw.githubusercontent.com/manishb20/Tv-logos/main/news18kerala.png"},
+        {"name": "MediaOne", "url": "https://anettv.tulnit.workers.dev/mediaone_live_https/index.m3u8", "logo": "https://raw.githubusercontent.com/manishb20/Tv-logos/main/mediaone.png"},
+        {"name": "Janam TV", "url": "https://anettv.tulnit.workers.dev/janamtv_live_https/index.m3u8", "logo": "https://raw.githubusercontent.com/manishb20/Tv-logos/main/janamtv.png"}
     ]
     
-    # തടസ്സമില്ലാതെ പ്ലേ ആകാൻ വേണ്ടിയുള്ള പുതിയ സെർവർ ബേസ് ടോക്കൺ ലിങ്ക്
-    # (ഓരോ 20 മിനിറ്റിലും ബോട്ട് ഈ പുതിയ സ്ട്രീം വച്ചാണ് ലിങ്ക് അപ്ഡേറ്റ് ചെയ്യുക)
-    token_base = "https://m3uplay.com/jio/live.mpd?wid="
-    
     for ch in channels:
-        # പുതിയ പ്ലേബാക്ക് ലിങ്ക് ഫോർമാറ്റ്
-        live_url = f"https://anet.keralive.workers.dev/v1/master/be381fcfb351ee2b450537f5bbf1e89324083a30/{ch['id']}/index.m3u8"
-        
         m3u_content += f'#EXTINF:-1 tvg-logo="{ch["logo"]}" group-title="Malayalam", {ch["name"]}\n'
         m3u_content += "#EXTVLCOPT:http-user-agent=AppleCoreMedia/1.0.0.16G77 (iPhone; U; CPU iPhone OS 12_4 like Mac OS X; en_us)\n"
-        m3u_content += "#EXTVLCOPT:http-referrer=https://anet.keralive.workers.dev/\n"
-        m3u_content += "#EXTVLCOPT:http-origin=https://anet.keralive.workers.dev\n"
-        m3u_content += f"{live_url}|User-Agent=AppleCoreMedia/1.0.0.16G77 (iPhone; U; CPU iPhone OS 12_4 like Mac OS X; en_us)&Referer=https://anet.keralive.workers.dev/\n\n"
+        m3u_content += "#EXTVLCOPT:http-referrer=https://tulnit.com/\n"
+        m3u_content += f"{ch['url']}|User-Agent=AppleCoreMedia/1.0.0.16G77 (iPhone; U; CPU iPhone OS 12_4 like Mac OS X; en_us)&Referer=https://tulnit.com/\n\n"
         
     with open("playlist.m3u", "w", encoding="utf-8") as f:
         f.write(m3u_content)
-    print("Success: All Malayalam Channels with Logos Configured Successfully!")
+    print("Success: Final Verified Malayalam Playlist Built Perfectly!")
 
 except Exception as e:
     print(f"Error: {e}")
